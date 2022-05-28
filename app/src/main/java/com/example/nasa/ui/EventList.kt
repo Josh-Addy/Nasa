9636b68de403d4636a959a39d307a274216c2b41
Josh-Addy <joshuaditya3011@gmail.com>
2022-05-28 10:43:51 +0530

    Structural update

---
+++ app/src/main/java/com/example/nasa/ui/EventList.kt
@@ -0,0 +1,12 @@
+package com.example.nasa.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.nasa.R
+
+class EventList : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_event_list)
+    }
+}
